eabedc50dbd59a570e76a023e5d7dfc3b4dde74b
Aromal S <aromalsanthosh@protonmail.com>
2021-05-16 20:29:08 +0530

    update-main.js✅

--- main.js
+++ main.js
@@ -9,6 +9,22 @@ client.once('ready',()=>{
     console.log('CoWIBot Is Online !');
 })
 
+var districtsKerala = [{ "district_id": 301, "district_name": "Alappuzha" },
+                    { "district_id": 298, "district_name": "Kollam" },
+                    { "district_id": 296, "district_name": "Thiruvananthapuram" },
+                    { "district_id": 303, "district_name": "Thrissur" },
+                    { "district_id": 304, "district_name": "Kottayam" },
+                    { "district_id": 305, "district_name": "Kozhikode" },
+                    { "district_id": 302, "district_name": "Malappuram" },
+                    { "district_id": 307, "district_name": "Ernakulam" },
+                    { "district_id": 306, "district_name": "Idukki" },
+                    { "district_id": 297, "district_name": "Kannur" },
+                    { "district_id": 295, "district_name": "Kasaragod" },
+                    { "district_id": 308, "district_name": "Palakkad" },
+                    { "district_id": 300, "district_name": "Pathanamthitta" },
+                    { "district_id": 299, "district_name": "Wayanad" }]
+
+
 client.on('message',message=>{
     //checks if the message not contain prefix or author is bot, condition true =>no action done
     if (!message.content.startsWith(prefix) || message.author.bot) return;
@@ -23,6 +39,16 @@ client.on('message',message=>{
 
     }else if(command == 'login'){
         message.channel.send('https://selfregistration.cowin.gov.in/')
+    }else if (command == 'help'){
+        message.channel.send(new Discord.MessageEmbed()
+						.setColor('#00aad2')
+						.setTitle('Help')
+						.addFields(
+							{ name: '-login', value: 'Hyperlink to official cowin website' },
+							{ name: '-ping', value: 'Check if bot is working' },
+							{ name: '-help',value:'List All Commands'}
+                            { name: '-check',value:'Check for Available Vaccine slot'}
+						));
     }
 
 

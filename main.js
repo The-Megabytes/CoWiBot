308fc259afe99bc0769fb3ca29eb6c6241317dda
Aromal S <aromalsanthosh@protonmail.com>
2021-05-16 10:40:39 +0530

    create-main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready',()=>{
+    console.log('CoWIBot Is Online !');
+})
+
+
+
+client.login('ODQzMzQ5MTE1NjA5NDE1NzAx.YKCkEQ.ynFr9Yh1wtGU8cf843LqUsVE3fo');

cd39f247fbb7904188d03ae423950f16310e0676
Aromal S <aromalsanthosh@protonmail.com>
2021-05-16 11:18:22 +0530

    Added,Basic Commands, setup env with dotenv package✅

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
+require('dotenv').config();
+
 const Discord = require('discord.js');
 
 const client = new Discord.Client();
-
+const prefix ='-';
 
 client.once('ready',()=>{
     console.log('CoWIBot Is Online !');
 })
 
+client.on('message',message=>{
+    //checks if the message not contain prefix or author is bot, condition true =>no action done
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+    //slices the command
+    const args =message.content.slice(prefix.length).split(/ +/);
+    const command=args.shift().toLowerCase();
 
 
-client.login('ODQzMzQ5MTE1NjA5NDE1NzAx.YKCkEQ.ynFr9Yh1wtGU8cf843LqUsVE3fo');
+    if(command === 'ping'){
+        message.channel.send('pong!');
+
+    }else if(command == 'login'){
+        message.channel.send('https://selfregistration.cowin.gov.in/')
+    }
+
+
+
+
+})
+
+
+
+client.login(process.env.BOTTOKEN);

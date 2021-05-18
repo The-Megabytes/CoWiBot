5cab3aa9781dbd6eeb6e132ef6ac6829eaa1bb93
Aromal S <aromalsanthosh@protonmail.com>
2021-05-18 19:22:50 +0530

    revert me here to retrieve old commmand management 〽

--- main.js
+++ main.js
@@ -30,45 +30,74 @@ client.once('ready',()=>{
 
 
 
-client.on('message',message=>{
-    //checks if the message not contain prefix or author is bot, condition true =>no action done
+// client.on('message',message=>{
+//     //checks if the message not contain prefix or author is bot, condition true =>no action done
+//     if (!message.content.startsWith(prefix) || message.author.bot) return;
+    
+//     //slices the command
+//     const args =message.content.slice(prefix.length).split(/ +/);
+//     const command=args.shift().toLowerCase();
+
+    
+//     // try {
+// 	// 	client.commands.get(command).execute(message, args);
+// 	// } catch (error) {
+// 	// 	console.error(error);
+// 	// 	message.reply('there was an error trying to execute that command!');
+// 	// }
+
+//     if(command === 'ping'){
+
+//         client.commands.get('ping').execute(message, args);
+
+//     }else if(command == 'link'){
+
+//         client.commands.get('link').execute(message,args);
+
+//     }else if (command == 'help'){
+
+//         client.commands.get('help').execute(message,args);
+//     }
+//     else if (command == 'register'){
+
+//         client.commands.get('register').execute(message,args);
+//     }
+
+
+
+
+
+
+
+// });
+
+
+client.on("message", (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
-    
-    //slices the command
-    const args =message.content.slice(prefix.length).split(/ +/);
-    const command=args.shift().toLowerCase();
 
-    
-    // try {
-	// 	client.commands.get(command).execute(message, args);
-	// } catch (error) {
-	// 	console.error(error);
-	// 	message.reply('there was an error trying to execute that command!');
-	// }
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-    if(command === 'ping'){
+    if (!client.commands.has(commandName)) return;
 
-        client.commands.get('ping').execute(message, args);
+    const command = client.commands.get(commandName);
 
-    }else if(command == 'link'){
+    if (command.arg && !args.length) {
+        let reply = `You didnt provide any arguments ${message.author}`;
 
-        client.commands.get('link').execute(message,args);
+        if (command.usage) {
+            reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
 
-    }else if (command == 'help'){
-
-        client.commands.get('help').execute(message,args);
-    }
-    else if (command == 'register'){
-
-        client.commands.get('register').execute(message,args);
+        return message.channel.send(reply);
     }
 
-
-
-
-
-
-
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply("there was an error trying to execute that command!");
+    }
 });
 
 client.login(process.env.BOTTOKEN);
